09fa0009e9fd57365b9e064052c67f614c05aeaf
Ramil <zarram89@gmail.com>
2025-10-20 13:14:18 +0500

    1.6 Добавит модуль «Кнопка добавления задачи»

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log('Hello');
+import NewTaskButtonView from './view/new-task-button-view.js';
+import {render} from './render.js';
+
+const sitemMainElement = document.querySelector('.main');
+const siteHeaderElement = sitemMainElement.querySelector('.main__control');
+
+render(new NewTaskButtonView(), siteHeaderElement);
