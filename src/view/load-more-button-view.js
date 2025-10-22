60cbcb86e6c671d107952b6d7c4eb19f81c83813
Ramil <zarram89@gmail.com>
2025-10-22 09:47:00 +0500

    4.1.6 Добавит логику допоказа задач. WIP

--- src/view/load-more-button-view.js
+++ src/view/load-more-button-view.js
@@ -1,11 +1,23 @@
 import AbstractView from '../framework/view/abstract-view.js';
-
 function createLoadMoreButtonTemplate() {
   return '<button class="load-more" type="button">load more</button>';
 }
 
 export default class LoadMoreButtonView extends AbstractView {
+  #handleClick = null;
+
+  constructor({onClick}) {
+    super();
+    this.#handleClick = onClick;
+    this.element.addEventListener('click', this.#clickHandler);
+  }
+
   get template() {
     return createLoadMoreButtonTemplate();
   }
+
+  #clickHandler = (evt) => {
+    evt.preventDefault();
+    this.#handleClick();
+  };
 }
