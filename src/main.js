0e556a742d31982a45851a178f39c760846d7318
Ramil <zarram89@gmail.com>
2025-10-25 15:31:56 +0500

    4.2.2 Подключит моки фильтров

--- src/main.js
+++ src/main.js
@@ -3,17 +3,19 @@ import NewTaskButtonView from './view/new-task-button-view.js';
 import FilterView from './view/filter-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import TasksModel from './model/tasks-model.js';
+import {generateFilter} from './mock/filter.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
-
-render(new NewTaskButtonView(), siteHeaderElement);
-render(new FilterView(), siteMainElement);
 const tasksModel = new TasksModel();
-
 const boardPresenter = new BoardPresenter({
   boardContainer: siteMainElement,
   tasksModel
 });
+
+const filters = generateFilter(tasksModel.tasks);
+
+render(new NewTaskButtonView(), siteHeaderElement);
+render(new FilterView({filters}), siteMainElement);
 
 boardPresenter.init();
