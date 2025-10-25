65a1afc328f64723458595eb2e888e8c48a7c132
Ramil <zarram89@gmail.com>
2025-10-25 11:23:41 +0500

    2.3 Подключит модель

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import NewTaskButtonView from './view/new-task-button-view.js';
 import FilterView from './view/filter-view.js';
 import {render} from './render.js';
-import BoardPresenter from "./presenter/board-presenter.js";
+import BoardPresenter from './presenter/board-presenter.js';
+import TasksModel from './model/tasks-model.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
 render(new NewTaskButtonView(), siteHeaderElement);
 render(new FilterView(), siteMainElement);
-const boardPresenter = new BoardPresenter({boardContainer: siteMainElement});
+const tasksModel = new TasksModel();
+
+const boardPresenter = new BoardPresenter({
+  boardContainer: siteMainElement,
+  tasksModel
+});
 
 boardPresenter.init();
