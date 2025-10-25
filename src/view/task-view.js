51fe15b9bbf45c8db719aeaedd5cd5f7e795a54c
Ramil <zarram89@gmail.com>
2025-10-25 11:45:13 +0500

    2.4 Обучит компонент задачи принимать данные

--- src/view/task-view.js
+++ src/view/task-view.js
@@ -1,8 +1,13 @@
 import {createElement} from '../render.js';
+import {humanizeTaskDueDate} from '../utils.js';
 
-function createTaskTemplate() {
+function createTaskTemplate(task) {
+  const {color, description, dueDate} = task;
+
+  const date = humanizeTaskDueDate(dueDate);
+
   return (
-    `<article class="card card--black">
+    `<article class="card card--${color}">
       <div class="card__form">
         <div class="card__inner">
           <div class="card__control">
@@ -25,14 +30,14 @@ function createTaskTemplate() {
             </svg>
           </div>
           <div class="card__textarea-wrap">
-            <p class="card__text">Example default task with default color.</p>
+            <p class="card__text">${description}</p>
           </div>
           <div class="card__settings">
             <div class="card__details">
               <div class="card__dates">
                 <div class="card__date-deadline">
                   <p class="card__input-deadline-wrap">
-                    <span class="card__date">23 September</span>
+                    <span class="card__date">${date}</span>
                   </p>
                 </div>
               </div>
@@ -45,8 +50,12 @@ function createTaskTemplate() {
 }
 
 export default class TaskView {
+  constructor({task}) {
+      this.task = task;
+  }
+
   getTemplate() {
-    return createTaskTemplate();
+    return createTaskTemplate(this.task);
   }
 
   getElement() {
